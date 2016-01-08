b1b9a79e874690dfcd2a8b2aa7d425d6dbafbbe4
duttaoindril <duttaoindril@gmail.com>
2016-01-08 02:06:53 -0800

    added form func

--- js/main.js
+++ js/main.js
@@ -4,9 +4,23 @@ var slide = true;
 var time = 4000;
 var percent = 0;
 
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] === sParam)
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+    }
+    return "";
+};
+
 function initialize() {
-
-
+    var title = getUrlParameter('sub');
+    if(title == "true")
+        $("#title").html("Thank you for reaching out!");
     $(".button-collapse").sideNav();
     if($(document).width() > 992 && $(window).width() > 992 )
         $('ul.tabs').tabs('select_tab', 'home');
